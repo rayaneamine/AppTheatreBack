52e05d7f92cd24331372309b55910323df2858c3
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-07-17 14:14:38 +0200

    users collection deleted

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const {
-  getAllUsers,
-  getUserByEmail,
-  getUserById,
-  createNewUser,
-  updateUser,
-  deleteUser,
-} = require("../controllers/usersController.js");
+// const {
+//   getAllUsers,
+//   getUserByEmail,
+//   getUserById,
+//   createNewUser,
+//   updateUser,
+//   deleteUser,
+// } = require("../controllers/usersController.js");
 
-router.get("/", getAllUsers);
-router.get("/:id", getUserById);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
-router.get("/email/:email", getUserByEmail);
-router.post("/", createNewUser);
+// router.get("/", getAllUsers);
+// router.get("/:id", getUserById);
+// router.put("/:id", updateUser);
+// router.delete("/:id", deleteUser);
+// router.get("/email/:email", getUserByEmail);
+// router.post("/", createNewUser);
 
-module.exports = router;
+// module.exports = router;
